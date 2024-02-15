bc886a36f0c4e6e06d1dd8b963bbf625f4bbc8f0
romeror <romeror@TCS-0390>
2024-02-15 12:11:38 -0600

    Add project files.

---
+++ 5ex2/MonthName.cs
@@ -0,0 +1,57 @@
+﻿namespace _ch5ex2
+{
+    internal class MonthName
+    {
+        private int month;
+        private int daysInMonth;
+
+        public MonthName()
+        {
+
+        }
+
+        public MonthName(int monthName, int daysNumMonth)
+        {
+
+        }
+
+        public int MonthNum
+        {
+            get
+            {
+                return month;
+            }
+            set
+            {
+                month = value;
+            }
+        }
+
+        public int DaysInMonth
+        {
+            get
+            {
+                return daysInMonth;
+            }
+            set
+            {
+                daysInMonth = value; ;
+            }
+        }
+
+        public override string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Mnth()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NumberOfDaysInMonth()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ 5ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ch5ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static char InputMonthNum()
+        {
+            string inValue;
+            char monthNum;
+            Console.WriteLine("enter month number");
+            inValue = Console.ReadLine();
+            monthNum = Convert.ToChar(inValue);
+            return monthNum;
+        }
+}
