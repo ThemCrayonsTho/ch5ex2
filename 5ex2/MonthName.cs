98fc2548368f5d1d3ac8f2828689767f7dc55127
ThemCrayonsTho <bigmacchunk@outlook.com>
2024-02-15 12:53:23 -0600

    breh

--- 5ex2/MonthName.cs
+++ 5ex2/MonthName.cs
@@ -45,9 +45,52 @@
             
         }
 
-        public void Month()
+        public int Month()
         {
-            
+            string monthName;
+            switch (month)
+            {
+                case '1':
+                    monthName = "January";
+                    break;
+                case '2':
+                    monthName = "February";
+                    break;
+                case '3':
+                    monthName = "March";
+                    break;
+                case '4':
+                    monthName = "April";
+                    break;
+                case '5':
+                    monthName = "May";
+                    break;
+                case '6':
+                    monthName = "February";
+                    break;
+                case '7':
+                    monthName = "March";
+                    break;
+                case '8':
+                    monthName = "April";
+                    break;
+                case '9':
+                    monthName = "January";
+                    break;
+                case "10":
+                    monthName = "February";
+                    break;
+                case "11":
+                    monthName = "March";
+                    break;
+                case "12":
+                    monthName = "April";
+                    break;
+                default:
+                    monthName = "unknown";
+                    break;
+            }
+            return monthName;
         }
 
         public void NumberOfDaysInMonth()
